10bfba89096a978d7dc4d989f8fe3c48925cde6e
eungchan-kang <yeschan119@gmail.com>
2023-07-30 00:21:38 +0900

    build a layout

--- financial_app/lib/main.dart
+++ financial_app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,15 +9,44 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('WebFlix'),
-        ),
-        body: Center(
-          child: Text('Start building WebFlix!'),
+    return const MaterialApp(
+        home: Scaffold(
+      backgroundColor: Color(0XFF181818),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end, //vertical direction
+              children: [
+                Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.end, //horizontal direction
+                  children: [
+                    Text(
+                      'Hey, EungChan',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
         ),
       ),
-    );
+    ));
   }
 }
