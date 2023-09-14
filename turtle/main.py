0cc23c29d4701cec01dec901a368e4112f8b8b7f
eungchan-kang <yeschan119@gmail.com>
2023-09-14 23:36:31 +0900

    dashed line

--- turtle/main.py
+++ turtle/main.py
@@ -1,12 +1,18 @@
 from turtle import Turtle, Screen
+import tkinter as tk
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
 
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(90)
+#draw dashed line
+for _ in range(15):
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.pendown()
+    
+    #timmy_the_turtle.left(90)
 
 screen = Screen()
 screen.exitonclick()
