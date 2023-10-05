0ee2f28ccc0ea649226df159b5a23aa85276f879
eungchan-kang <yeschan119@gmail.com>
2023-10-06 00:29:36 +0900

    make ball

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,5 +1,8 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
+
 #Break the problem
 ##Create the screen
 ##create and move a paddle
@@ -21,7 +24,8 @@ screen.tracer(0)
 ## create and move a paddle
 l_paddle = Paddle(350, 0)
 r_paddle = Paddle(-350, 0)
-    
+
+ball = Ball()
 screen.listen()
 screen.onkey(l_paddle.go_up, "Up")
 screen.onkey(l_paddle.go_down, "Down")
@@ -29,7 +33,23 @@ screen.onkey(r_paddle.go_up, "w")
 screen.onkey(r_paddle.go_down, "s")
 #tracer를 껐기 때문에 매번 screen을 직접 update을 해주지 않으면 화면에 아무 것도 나타나지 않음
 game_is_on = True
+
+#화면을 갱신해줌
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    print(ball.xcor())
+    print()
+    print(ball.ycor())
+    if ball.xcor() > 290 and ball.ycor() >= 0:
+        new_x = ball.xcor() + 10
+        new_y = ball.ycor() - 10
+        ball.goto(new_x, new_y)
+    elif ball.xcor() > 290 and ball.ycor() < 0:
+        new_x = ball.xcor() + 10
+        new_y = ball.ycor() + 10
+        ball.goto(new_x, new_y)
+        
 
 screen.exitonclick()
