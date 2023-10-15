353a793615da5e00fdde0db20762e280bbcc2382
eungchan-kang <yeschan119@gmail.com>
2023-10-16 00:41:01 +0900

    각 주들의 이름을 입력할 때마다 지도 위에서 해당 주들의 위치를 나타내주는 함수 추가

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -1,7 +1,9 @@
 import turtle
 import pandas
 from show_states import ShowState
+import json
 
+NUM_OF_STATES = 50
 screen = turtle.Screen()
 screen.title("U.S stats game")
 image = "blank_states_img.gif"
@@ -9,27 +11,35 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-'''
-#화면에 마우스를 클릭하면 좌표를 리턴해주는 함수
-def get_mouse_click_coor(x, y):
-    print(x, y)
-
-turtle.onscreenclick(get_mouse_click_coor)
-'''
 states_info = pandas.read_csv("50_states.csv")
+state_list = states_info.state.to_list()
+
+with open('border_line.json', 'r') as json_file:
+    border_info = json.load(json_file)
+
 show_states = ShowState()
-answer_cnt = 0
+right_answer_cnt = 0
+guessed_state_list = []
+remained_states = []
 
 while True:
-    bar_title = str(answer_cnt) + "/50 States Correct"
+    bar_title = str(right_answer_cnt) + "/50 States Correct"
     input_state = screen.textinput(title=bar_title, prompt="What's another state's name?")
     
-    if input_state == None:
+    if input_state == None or right_answer_cnt >= NUM_OF_STATES:
+        remained_states = list(set(state_list) - set(guessed_state_list))
         break
-    for state, x, y in zip(states_info.state, states_info.x, states_info.y):
-        #title()은 첫 글자를 대문자로 나머지는 모두 소문자로 바꿔줌
-        if state.title() == input_state.title():
-            answer_cnt += 1
-            show_states.write_state_name(state_name=input_state.title(), posx=x, posy=y)
-    
+    #title()은 첫 글자를 대문자로 나머지는 모두 소문자로 바꿔줌
+    if input_state.title() in state_list:
+        right_answer_cnt += 1
+        guessed_state = states_info[states_info.state == input_state.title()]
+        show_states.write_state_name(state_name=input_state.title(),
+                                    posx=int(guessed_state.x), posy=int(guessed_state.y))
+        
+        show_states.paint_state(border_info[input_state.title()])
+        guessed_state_list.append(input_state)
+        
+remained_data = pandas.DataFrame(remained_states)
+remained_data.to_csv("remained_states.csv")
+
 turtle.mainloop()
--- us_states_game/show_states.py
+++ us_states_game/show_states.py
@@ -4,8 +4,17 @@ class ShowState(Turtle):
     def __init__(self):
         super().__init__()
         self.hideturtle()
-        self.penup()
         
     def write_state_name(self, state_name, posx, posy):
+        self.penup()
         self.goto(posx, posy)
         self.write(state_name)
+        
+    def paint_state(self, border_list):
+        self.goto(border_list[0][0], border_list[0][1])
+        self.pendown()
+        self.fillcolor("yellow")
+        self.begin_fill()
+        for pos in border_list:
+            self.goto(pos[0], pos[1])
+        self.end_fill()
